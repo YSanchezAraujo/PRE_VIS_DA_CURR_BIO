57fca24ddbea834c0e7091465b182d001b07b4db
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2024-08-09 19:08:29 -0400

    Update postprocess.jl

--- pipeline/base/postprocess.jl
+++ pipeline/base/postprocess.jl
@@ -1,12 +1,15 @@
-include("utility.jl");
+include("/Users/ysa/Desktop/pipeline/base/utility.jl");
 using JLD2;
 
 # load results
-results = load("neural_results.jld2", "results");
+results = load("/Users/ysa/Desktop/pipeline/saved_results/neural_results.jld2", "results");
 kernel_norm = results.kernel_norm;
 
 mouse_ids = [collect(13:16); collect(26:43)];
 
-contra_stim_dms = [results_Knorm[mouse]["DLS"][dms_contra_map[mouse]] for mouse in mouse_ids];
-contra_stim_dms = reshape(hcat(contra_stim_dms...), 20, 4, 22);
+n_days = 20
+n_sim = 4
+n_mice = 22
+contra_stim_dms = [kernel_norm[mouse]["DMS"][dms_contra_map[mouse]] for mouse in mouse_ids];
+contra_stim_dms = reshape(hcat(contra_stim_dms...), n_days, n_sim, n_mice);
 avg_contra_dms = nanmean(contra_stim_dms, 3);
